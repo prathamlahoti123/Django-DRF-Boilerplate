5843721012475214782ad0d1cb4be99c0b4d1179
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-14 11:28:59 +0200

    Remove unused files

--- src/main/django/local.py
+++
@@ -1,9 +0,0 @@
-from main.env import env
-
-from .base import *
-
-DEBUG = env.bool("DJANGO_DEBUG", default=True)
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
-
-INSTALLED_APPS.append("silk")
-MIDDLEWARE.append("silk.middleware.SilkyMiddleware")
