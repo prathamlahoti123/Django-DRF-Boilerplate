d1537f97cf1754514c4ed6b115987148b15a60fe
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-14 10:24:16 +0200

    Update production settings

--- src/main/django/production.py
+++ src/main/django/production.py
@@ -1,6 +1,3 @@
-from main.env import env
-
 from .base import *
 
-DEBUG = env.bool("DJANGO_DEBUG", default=False)
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".elasticbeanstalk.com"])
+DEBUG = False
