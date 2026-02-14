8cbdc4feb2d5e05a132eb7171ebcadeba6d54038
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-14 23:05:40 +0200

    Add comments to supress mypy errors

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin  # type: ignore[import-untyped]
 
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(ModelAdmin):
+class UserAdmin(ModelAdmin):  # type: ignore[misc]
   """Custom admin for User model using django-unfold package."""
