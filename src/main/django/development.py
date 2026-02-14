70821f728ffcf200481598bd2cba9616c85f2954
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-14 11:28:19 +0200

    Add settings for Django-Debug-Toolbar

--- src/main/django/development.py
+++ src/main/django/development.py
@@ -1,4 +1,16 @@
 from .base import *
 
-INSTALLED_APPS.append("silk")
+INSTALLED_APPS.extend(
+  [
+    "silk",
+    "debug_toolbar",
+  ]
+)
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+# SilkyMiddleware can be simply appended to the list of middlewares
 MIDDLEWARE.append("silk.middleware.SilkyMiddleware")
+
+# DebugToolbarMiddleware should be as early as possible in the list of middlewares
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
