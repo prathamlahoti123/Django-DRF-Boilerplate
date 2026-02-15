2e1c20118bcf5dad63ddb3ab93beec716d3466eb
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-15 16:43:19 +0200

    Add new constant to specify stage environment

--- src/main/constants.py
+++ src/main/constants.py
@@ -1,6 +1,7 @@
 class DjangoSettingsModule:
-  """Django settings module name."""
+  """Supported environment settings modules for Django."""
 
   DEVELOPMENT = "main.django.development"
   PRODUCTION = "main.django.production"
+  STAGE = "main.django.stage"
   TEST = "main.django.test"
