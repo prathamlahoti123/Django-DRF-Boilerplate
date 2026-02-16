180485e3952425542a4066f908ca88edf8c89be1
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-16 17:42:27 +0200

    Change name of the environment variables

--- src/main/settings/unfold.py
+++ src/main/settings/unfold.py
@@ -1,6 +1,6 @@
 from main.env import env
 
 UNFOLD = {
-  "SITE_TITLE": env.str("UNFOLD_SITE_TITLE", "Django Admin Title"),
-  "SITE_HEADER": env.str("UNFOLD_SITE_HEADER", "Django Admin Header"),
+  "SITE_TITLE": env.str("DJANGO_ADMIN_UI_TITLE", "Django Admin Title"),
+  "SITE_HEADER": env.str("DJANGO_ADMIN_UI_HEADER", "Django Admin Header"),
 }
