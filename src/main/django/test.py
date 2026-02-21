3b3756b70b7600636f2b735960b5df97a48a0e43
prathamlahoti123 <prathamlahoti@duck.com>
2026-02-21 17:54:45 +0200

    Set CACHES and DATABASES settings for test environment

--- src/main/django/test.py
+++ src/main/django/test.py
@@ -1,1 +1,14 @@
 from .base import *
+
+CACHES = {
+  "default": {
+    "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+  }
+}
+
+DATABASES = {
+  "default": {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": ":memory:",
+  }
+}
