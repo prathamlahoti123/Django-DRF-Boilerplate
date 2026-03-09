13678b8d018e5327a8e96e74f679e507b5d31b20
prathamlahoti123 <prathamlahoti@duck.com>
2026-03-09 10:47:27 +0200

    Set default admin username and email

--- src/main/settings/custom.py
+++ src/main/settings/custom.py
@@ -2,8 +2,8 @@ import secrets
 
 from main.env import env
 
-DJANGO_ADMIN_USERNAME = env.str("DJANGO_ADMIN_USERNAME")
-DJANGO_ADMIN_EMAIL = env.str("DJANGO_ADMIN_EMAIL")
+DJANGO_ADMIN_USERNAME = env.str("DJANGO_ADMIN_USERNAME", default="admin")
+DJANGO_ADMIN_EMAIL = env.str("DJANGO_ADMIN_EMAIL", default="admin@example.com")
 DJANGO_ADMIN_PASSWORD = env.str(
   "DJANGO_ADMIN_PASSWORD",
   default=secrets.token_urlsafe(10),
