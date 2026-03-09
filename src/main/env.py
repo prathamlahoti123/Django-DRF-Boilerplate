b0009cc268cf10bf52802afb5e6a533881bbac50
prathamlahoti123 <prathamlahoti@duck.com>
2026-03-09 11:16:31 +0200

    Restore step to read env vars from file

--- src/main/env.py
+++ src/main/env.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
 import environ  # type: ignore[import-untyped]
 
 env = environ.Env()
+env_fp = Path(__file__).parents[2] / "config" / ".env"
+if env_fp.exists():
+  environ.Env.read_env(env_fp)
